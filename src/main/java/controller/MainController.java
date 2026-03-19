5afe309224c7edcf034f45895b65b43d1a6578a7
Akil1928 <akilwatsonch1928@gmail.com>
2026-03-18 18:44:10 -0600

    akil

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,7 +1,49 @@
 package controller;
 
-public class MainController
-{
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
     @javafx.fxml.FXML
-    public void initialize() {
-    }}
+    private Canvas canvasTree;
+    @javafx.fxml.FXML
+    private Slider sliderFactN;
+    @javafx.fxml.FXML
+    private Label lblFact;
+    @javafx.fxml.FXML
+    private Button btnFactCalc;
+    @javafx.fxml.FXML
+    private Button btnFactReset;
+    @javafx.fxml.FXML
+    private Label lblComplexity;
+    @javafx.fxml.FXML
+    private Label lblFactCalls;
+    @javafx.fxml.FXML
+    private ListView listSteps;
+    @javafx.fxml.FXML
+    private Label lblFactResult;
+//Jav
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @Deprecated
+    public void calcularOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void limpiarOnAction(ActionEvent actionEvent) {
+    }
+}
